85c2160f81edb417fd7840d6d6fda826bfa004a1
Md Aftab Uddin <aftabhasan7856@gmail.com>
2026-01-11 10:32:53 +0600

    Make navbar stricky

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -16,9 +16,9 @@ const handleLogout = ()=>{
 
 
     return (
-        <div>
+        <div className="sticky top-0 z-50 bg-base-100">
             <div className="navbar bg-base-100  max-w-7xl mx-auto">
-  <div className="navbar-start">
+  <div className="navbar-start ">
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
@@ -88,7 +88,7 @@ const handleLogout = ()=>{
     </div>:<Link to='/auth/login' className="btn btn-primary">Login</Link>}
   </div>
   </div>
-  <hr />
+  
 </div>
       
     )
